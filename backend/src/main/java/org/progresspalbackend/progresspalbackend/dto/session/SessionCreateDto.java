c07297e7ced35eefd37e08581d2857b8de640cc7
yassirrahdouni <yassirrahdouni@gmail.com>
2026-01-27 18:17:51 +0100

    feat(session): add validation to session create DTO

--- backend/src/main/java/org/progresspalbackend/progresspalbackend/dto/session/SessionCreateDto.java
+++ backend/src/main/java/org/progresspalbackend/progresspalbackend/dto/session/SessionCreateDto.java
@@ -1,13 +1,21 @@
 package org.progresspalbackend.progresspalbackend.dto.session;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.progresspalbackend.progresspalbackend.domain.Visibility;
 
 import java.util.UUID;
 
 public record SessionCreateDto(
 //        UUID userId,
+        @NotNull(message = "ActivityTypeId is required")
         UUID activityTypeId,
+
+        @Size(max = 120, message = "Title must be at least")
         String title,
+
         String description,
+
+        @NotNull(message = "Visibility is required")
         Visibility visibility
 ) {}
