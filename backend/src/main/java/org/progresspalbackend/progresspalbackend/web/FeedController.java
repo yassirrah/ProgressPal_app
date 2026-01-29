620c573078ec4b7fb45282085b826946654cda3f
yassirrahdouni <yassirrahdouni@gmail.com>
2026-01-29 19:56:38 +0100

    test(feed): test feed api

--- backend/src/main/java/org/progresspalbackend/progresspalbackend/web/FeedController.java
+++ backend/src/main/java/org/progresspalbackend/progresspalbackend/web/FeedController.java
@@ -2,9 +2,7 @@ package org.progresspalbackend.progresspalbackend.web;
 
 import org.progresspalbackend.progresspalbackend.dto.feed.FeedSessionDto;
 import org.progresspalbackend.progresspalbackend.service.SessionService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
